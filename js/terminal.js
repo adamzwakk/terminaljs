8d93e7d0908884fe19a84b21db8fe4c349f8e4ba
Adam Zwakenberg <shamist@gmail.com>
2018-08-22 10:52:37 -0400

    added output timeline function

--- js/terminal.js
+++ js/terminal.js
@@ -7,6 +7,7 @@ function Terminal(options){
 
 	this.app = new PIXI.Application(800,600);
 	this.command = '';
+	this.allowCommands = false;
 	this.prefix = this.user+'@'+this.server+':'+this.path+'$ ';
 	this.outputLines = [];
 	this.activeLine = 0;
@@ -19,6 +20,7 @@ function Terminal(options){
 
 	this.startup = function(){
 		this.app.stage.addChild(this.textContainer);
+		document.body.appendChild(this.app.view);
 		this.textContainer.filterArea = new PIXI.Rectangle(0,0,800,600);
 		this.textContainer.filters = [new PIXI.filters.CRTFilter({
 			// options at https://github.com/pixijs/pixi-filters/blob/master/filters/crt/src/CRTFilter.js
@@ -31,36 +33,42 @@ function Terminal(options){
 			text.x = 8;
 			text.y = (this.columns*i)+8;
 			this.outputLines.push(text);
-		}
-
-		document.body.appendChild(this.app.view);
+		}		
 
 		document.addEventListener('keydown', (event) => {
-			var keycode = event.keyCode;
-			var keyName = event.key;
+			if(this.allowCommands){	
+				var keycode = event.keyCode;
+				var keyName = event.key;
 
-			switch(keycode){
-				case 13:
-					this.sendCommand(this.command);
-					break;
-				case 8:
-					this.command = this.command.substr(0,this.command.length-1);
-					break;
-				default:
-					if (keyName.length === 1 && /[a-zA-Z0-9-_/ ]/.test(keyName)){
-						this.command += keyName;
-					}
+				switch(keycode){
+					case 13:
+						this.sendCommand(this.command);
+						break;
+					case 8:
+						this.command = this.command.substr(0,this.command.length-1);
+						break;
+					default:
+						if (keyName.length === 1 && /[a-zA-Z0-9-_/ ]/.test(keyName)){
+							this.command += keyName;
+						}
+				}
+				this.textContainer.removeChild(this.outputLines[this.activeLine]);
+				this.outputLines[this.activeLine].text = this.prefix+this.command;
+				this.textContainer.addChild(this.outputLines[this.activeLine]);
 			}
-			this.textContainer.removeChild(this.outputLines[this.activeLine]);
-			this.outputLines[this.activeLine].text = this.prefix+this.command;
-			this.textContainer.addChild(this.outputLines[this.activeLine]);
 		});
 
 		var that = this;
-		if(Array.isArray(this.bootSeq)){
-
+		if(typeof this.bootSeq === 'object'){
+			setTimeout(function(){
+				that.playOutputTimeline(that.bootSeq,function(){
+					that.bootOS();
+				});
+			},500);
 		} else {
-			setTimeout(function(){that.initTerminal(); that.readyForInput();},500);	
+			setTimeout(function(){
+				that.bootOS();
+			},500);
 		}
 	}
 
@@ -76,7 +84,7 @@ function Terminal(options){
 	}
 
 	this.initTerminal = function(){
-		for (i = 0; i < this.outputLines.length-1; i++) { 
+		for (i = 0; i < this.outputLines.length; i++) { 
 			this.outputLines[i].text = '';
 			this.textContainer.removeChild(this.outputLines[i]);
 		}
@@ -86,10 +94,10 @@ function Terminal(options){
 	}
 
 	this.readyForInput = function(){
-		var that = this;
-		this.app.ticker.add(function(delta) {
-			//that.drawCommandOutput();
-		});
+		// var that = this;
+		// this.app.ticker.add(function(delta) {
+		// 	//that.drawCommandOutput();
+		// });
 	}
 
 	this.sendCommand = function(){
@@ -108,5 +116,40 @@ function Terminal(options){
 		this.command = '';
 		this.activeLine++;
 		this.drawNewBashLine();
+	}
+
+	this.randomNumber = function(min, max) {
+	    return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	this.bootOS = function(){
+		this.initTerminal(); 
+		this.readyForInput();
+		this.allowCommands = true;
+	}
+
+	this.playOutputTimeline = function(obj,callback){
+		this.allowCommands = false;
+		var timed = 0;
+		var that = this;
+		Object.keys(obj).forEach(function(key) {
+			if(key === 'lines'){
+				var length = Object.keys(obj[key]).length;
+				var lines = obj[key];
+				var lineCount = 0;
+				Object.keys(lines).forEach(function(time) {
+					lineCount++;
+					var l = lines[time];
+					setTimeout(function(lineCount){
+						that.drawLine(l);
+						if(lineCount === length)
+						{
+							callback();
+							that.allowCommands = true;
+						}
+					},parseInt(time),lineCount);
+				});
+			}
+		});
 	}
 }
